d89283e50e5105319ecccb8b0891b567ff10a3fa
Rozovits Mihály <rozovits.mihaly@gmail.com>
2025-02-17 22:52:51 +0100

    preparingto convert to proper vue component package

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { type Bit, type Bitmap, Pen, type PenColor, Tool, PENS } from './Bitmap'
+import BitmapEditor from './BitmapEditor.vue'
+import BitmapToolbox from './BitmapToolbox.vue'
+
+export { BitmapEditor, BitmapToolbox, type Bit, type Bitmap, Pen, type PenColor, Tool, PENS}
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { fileURLToPath, URL } from 'node:url'
+import { dirname, resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(dirname(fileURLToPath(import.meta.url)), 'src/main.ts'),
+      name: 'BitmapEditor',
+      // the proper extensions will be added
+      fileName: 'vue-bitmap-editor',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
+
+
